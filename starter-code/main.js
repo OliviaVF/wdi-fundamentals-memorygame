b3b91bf0c8cf3a6c5e4a105137798fa185ecba22
Olivia Vaughan-Fowler <OliviaVaughanFowler@olivias-air.home>
2017-01-12 14:38:58 +0000

    createCards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,28 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+/*if ((cardOne === cardTwo) || (cardThree === cardFour)) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+if (cardThree === cardFour) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+*/
+var createCards = function(){
+var gameBoard = document.getElementById('game-board');
+for(var i = 1; i <= 4; i++){
+	//Create newCard as a Div
+	var newCard = document.createElement('div')
+	//Add Class of .card to the NewCard
+	newCard.className = 'card';
+	//Append card to the board
+	gameBoard.appendChild(newCard);
+	}
+}
+
+createCards();
+
