c4aeaeb32a18a33eb44625a4b55bed613651671d
Olivia Vaughan-Fowler <OliviaVaughanFowler@olivias-air.home>
2017-01-13 16:57:15 +0000

    completed game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,30 +1,48 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-/*if ((cardOne === cardTwo) || (cardThree === cardFour)) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
-if (cardThree === cardFour) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
-*/
-var createCards = function(){
-var gameBoard = document.getElementById('game-board');
-for(var i = 1; i <= 4; i++){
-	//Create newCard as a Div
-	var newCard = document.createElement('div')
-	//Add Class of .card to the NewCard
-	newCard.className = 'card';
-	//Append card to the board
-	gameBoard.appendChild(newCard);
+
+var cards = ['queen','queen','king','king']
+var cardsInPlay = []
+	
+var board = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		board.appendChild(cardElement);
+		board.appendChild(cardElement);
 	}
 }
 
-createCards();
+var isTwoCards = function () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = '<img src="king.png" alt="King of Hearts"/>';
+		} else {
+			this.innerHTML = '<img src="queen.png" alt="Queen of CLubs"/>';
+		}	
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
+}
+
+var isMatch = function(cards) {
+		if (cards[0] === cards[1]) {
+			setTimeout(function() {	
+			alert("You found a match!");
+			} ,1000);
+		} else {
+			setTimeout(function() {	
+			alert("Sorry, try again.");
+			} ,1000);
+		}
+}
+
+createBoard();
+
+
 
